17b89256874abf1cdedf4fb72d9ba51751e6a5be
chad63e@gmail.com <chad63e@gmail.com>
2024-01-29 15:45:29 +0000

    Edited Form1; Deleted ServerModule1, Docs/QuickStart.md and Docs/.gitkeep

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,6 +10,3 @@ class Form1(Form1Template):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
-        doc_url = f"{anvil.server.get_app_origin()}/_/theme/Docs/QuickStart.md"
-        with open(doc_url, "r") as file:
-            self.rich_text_1.content = file.read()
